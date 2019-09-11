4f5f0ad0d5e2637a19cbeb849954e03ce95a2cd6
estudiante <estudiante@SC310E13.local>
2019-09-10 19:40:23 -0500

    skeleton working

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', function() {
 
-    var server = 'http://192.168.115.15:1337';
+    var server = 'http://192.168.115.13:1337';
     var promise = fetch(server + '/products');
 
+    document.body.classList.add('loading');
     promise
         .then(function (response) {
             return response.json();
@@ -11,6 +12,7 @@ window.addEventListener('load', function() {
             console.log(response);
             
             var productsList = document.querySelector('.products-list');
+            document.body.classList.remove('loading');
             
             var template = document.querySelector('#product-item');
 
@@ -18,9 +20,22 @@ window.addEventListener('load', function() {
                 var product = document.importNode(template.content, true);
                 product.querySelector('img').setAttribute('src', server + elem.images[0].url);
                 product.querySelector('.card-title').innerText = elem.name;
+
+                product.querySelector('.card-text').innerText = elem.price;
                 productsList.append(product);
             });
 
         });
 
+    var searchBtn = document.querySelector('.btn');
+    var searchInput = document.querySelector('input');
+    function showSkeleton (){
+        document.body.classList.add('loading');
+
+        setTimeout(function(){
+            document.body.classList.remove('loading');
+        }, 1000);
+    }
+    searchBtn.addEventListener('click', showSkeleton);
+    searchInput.addEventListener('input', showSkeleton);
 });
